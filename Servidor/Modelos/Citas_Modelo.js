f9d24887a7dac5adbf9f261ddd7e60d0456d5ee5
Guile9197 <guille.gnz.27.1997@gmail.com>
2023-07-02 21:24:29 -0600

    hola

--- Servidor/Modelos/Citas_Modelo.js
+++ Servidor/Modelos/Citas_Modelo.js
@@ -15,7 +15,8 @@ class Citas_Modelo{
     agregar_cita(){
         //Logica y Sentencia SQL para relizar x operación sobre los datos
         return new Promise((resolve, reject) => {
-            let SentenciaSQL = `INSERT INTO citasautolavado(id_usuario,tipoVehiculo,fecha,hora) VALUES (${parseInt(this.id_usuario)},"${this.tipoVehiculo}",'${this.fecha}','${this.hora}')`
+            let SentenciaSQL = `INSERT INTO citasautolavado(id_usuario,tipoVehiculo,fecha,hora) 
+            VALUES (${parseInt(this.id_usuario)},"${this.tipoVehiculo}",'${this.fecha}','${this.hora}')`
             connection.query(`${SentenciaSQL}`, (err, rows) => {
                 if (err || rows.length == 0) return reject(err)
                 return resolve(rows)
@@ -24,9 +25,56 @@ class Citas_Modelo{
 
     }
 
-    //Más funciones aquí...
+    consultar_cita(){
+        //Logica y Sentencia SQL para relizar x operación sobre los datos
+        return new Promise((resolve, reject) => {
+            let SentenciaSQL = `SELECT * FROM citasautolavado WHERE id_cita = ${parseInt(this.id_cita)}`
+            connection.query(`${SentenciaSQL}`, (err, rows) => {
+                if (err || rows.length == 0) return reject(err)
+                return resolve(rows)
+            })
+        })
+
+    }
+
+    modificar_cita(){
+        //Logica y Sentencia SQL para relizar x operación sobre los datos
+        return new Promise((resolve, reject) => {
+            let SentenciaSQL = `UPDATE citasautolavado SET tipoVehiculo = "${this.tipoVehiculo}", 
+            fecha = "${this.fecha}", hora = "${this.hora}" WHERE id_usuario = ${parseInt(this.id_usuario)}';`
+            connection.query(`${SentenciaSQL}`, (err, rows) => {
+                if (err || rows.length == 0) return reject(err)
+                return resolve(rows)
+            })
+        })
+
+    }
+
+
+
+    //Más funciones aquí...(ESTOS SON DE EXPERIMENTO)------------------------------------------
+    eliminar_cita(){
+        //Logica y Sentencia SQL para relizar x operación sobre los datos
+        return new Promise((resolve, reject) => {
+            let SentenciaSQL = `DELETE FROM citasautolavado WHERE id_usuario = ${parseInt(this.id_usuario)} 
+            AND tipoVehiculo = "${this.tipoVehiculo}" AND fecha = '${this.fecha}' AND hora = '${this.hora}';`
+            connection.query(`${SentenciaSQL}`, (err, rows) => {
+                if (err || rows.length == 0) return reject(err)
+                return resolve(rows)
+            })
+        })
+
+
+
+        
+        
+
+    }
+    //-----------------------------------------------------------------------------------------
 
 }
+
+
 
 
 module.exports = Citas_Modelo;
